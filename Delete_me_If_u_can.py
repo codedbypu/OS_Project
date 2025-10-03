b3d7f735d2a4e2382d11a88cda70f79151f79d16
ImMarchuuu <kanokpon4828@gmail.com>
2025-10-03 23:24:02 +0700

    My_Web

---
+++ Delete_me_If_u_can.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>สวัสดี! นี่คือเว็บแรกของฉัน</h1>"
+
+@app.route("/about")
+def about():
+    return "<p>หน้านี้คือ About Page</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
