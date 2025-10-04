2aae93db67659e00e8d39851954776af84b90e4c
allproject-pu <puwanat17848@gmail.com>
2025-10-05 03:43:26 +0700

    ฟหก

--- Pu_Part/Client_Os.java
+++ Pu_Part/Client_Os.java
@@ -3,35 +3,16 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client_Os {
-    private String clientId;
-    private BlockingQueue<String> replyQueue = new LinkedBlockingQueue<>();
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 5000;
 
     public static void main(String[] args) throws Exception {
-        private BlockingQueue<String> replyQueue = new LinkedBlockingQueue<>();
-        Random random = new Random();
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String clientname = sc.nextLine();
-        int clientId = random.nextInt();
-
-        Socket socket = new Socket("localhost", 5000); // ระบุปลายทาง
+        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 
-        while (true) {
-            System.out.println("============================= All Instructions =============================\n" +
-                    "DM <receiver_name> <message>   # send a direct message to a specific friend\r\n" +
-                    "JOIN <#room_name>              # join a chat room\r\n" +
-                    "WHO <#room_name>               # see who is in the room\r\n" +
-                    "SAY <#room_name> <message>     # send a message to everyone in the room\r\n" +
-                    "LEAVE <#room_name>             # leave the room\r\n" +
-                    "QUIT                           # exit the program \r\n" +
-                    "============================================================================");
-            System.out.print("Instruction: ");
-            String line = sc.nextLine();
-            if (line.equalsIgnoreCase("QUIT"))
-                break;
-        }
-        socket.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String clientName = sc.nextLine();
     }
 }
